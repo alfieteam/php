c6168e22b604a00d4ceb365d740f4b831cb65f27
alfieteam <alfieteam1993@gmail.com>
2017-12-29 12:12:52 +0200

    myshop.ru ver. 1.2

--- www/index.php
+++ www/index.php
@@ -1,3 +1,11 @@
 <?php
-	phpinfo();
+//Определяем с каким контроллером будем работать
+$controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : 'Index';
+echo 'Подключенный php файл (контроллер) = '.$controllerName.'<br>';
+//Определяем с какой функцией будем работать
+$actionName = isset($_GET['action']) ? $_GET['action'] : 'index';
+echo 'Функция формирующая страницу (Экшен) = '.$actionName.'<br>';
+//Подключаем контроллер
+include_once '../controllers/'.$controllerName.'Controller.php';
+testAction();
 ?>
